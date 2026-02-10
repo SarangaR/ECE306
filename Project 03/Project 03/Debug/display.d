d31524a38290b2e053a75a6e3c0232553681ef7e
SarangaR <sarnga.raj@gmail.com>
2026-02-09 19:05:18 -0500

    forward movement

---
+++ Project 03/Project 03/Debug/display.d
@@ -0,0 +1,33 @@
+# FIXED
+
+display.obj: ../display.c
+display.obj: C:/ti/ccs1281/ccs/ccs_base/msp430/include/msp430.h
+display.obj: C:/ti/ccs1281/ccs/ccs_base/msp430/include/msp430fr2355.h
+display.obj: C:/ti/ccs1281/ccs/ccs_base/msp430/include/in430.h
+display.obj: C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h
+display.obj: C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h
+display.obj: C:/ti/ccs1281/ccs/ccs_base/msp430/include/legacy.h
+display.obj: ../Include/functions.h
+display.obj: ../Include/LCD.h
+display.obj: ../Include/macros.h
+
+../display.c:
+
+C:/ti/ccs1281/ccs/ccs_base/msp430/include/msp430.h:
+
+C:/ti/ccs1281/ccs/ccs_base/msp430/include/msp430fr2355.h:
+
+C:/ti/ccs1281/ccs/ccs_base/msp430/include/in430.h:
+
+C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h:
+
+C:/ti/ccs1281/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h:
+
+C:/ti/ccs1281/ccs/ccs_base/msp430/include/legacy.h:
+
+../Include/functions.h:
+
+../Include/LCD.h:
+
+../Include/macros.h:
+
