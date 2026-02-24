2c5a324091bd9dfc199232cf7188d4caaebb050a
SarangaR <sarnga.raj@gmail.com>
2026-02-24 15:50:52 -0500

    Project 05 preliminary setup, upgrade to ccs 20.4

---
+++ Project 05/Include/motors.h
@@ -0,0 +1,17 @@
+//------------------------------------------------------------------------------
+//
+//  Description: This file contains the declarations for the functions that control the motors.
+//
+//  Saranga Rajagopalan
+//  Feb 2026
+//  Built with Code Composer Version: CCS12.4.0.00007_win64
+//------------------------------------------------------------------------------
+
+void allMotorsOff(void);
+void allMotorsOn(void);
+
+void leftMotorOff(void);
+void rightMotorOff(void);
+
+void leftMotorOn(void);
+void rightMotorOn(void);
---
+++ Project 05/led.c
@@ -0,0 +1,56 @@
+#include "msp430.h"
+#include "Include\functions.h"
+#include "Include\LCD.h"
+#include "Include\ports.h"
+#include "Include\macros.h"
+
+void Carlson_StateMachine(void)
+{
+  switch (Time_Sequence)
+  {
+  case 250: //
+    if (one_time)
+    {
+      Init_LEDs();
+      lcd_BIG_mid();
+      display_changed = 1;
+      one_time = 0;
+    }
+    Time_Sequence = 0; //
+    break;
+  case 200: //
+    if (one_time)
+    {
+      //          P1OUT &= ~RED_LED;            // Change State of LED 4
+      P6OUT |= GRN_LED; // Change State of LED 5
+      one_time = 0;
+    }
+    break;
+  case 150: //
+    if (one_time)
+    {
+      P1OUT |= RED_LED;  // Change State of LED 4
+      P6OUT &= ~GRN_LED; // Change State of LED 5
+      one_time = 0;
+    }
+    break;
+  case 100: //
+    if (one_time)
+    {
+      //          lcd_4line();
+      lcd_BIG_bot();
+      P6OUT |= GRN_LED; // Change State of LED 5
+      display_changed = 1;
+      one_time = 0;
+    }
+    break;
+  case 50: //
+    if (one_time)
+    {
+      one_time = 0;
+    }
+    break; //
+  default:
+    break;
+  }
+}
